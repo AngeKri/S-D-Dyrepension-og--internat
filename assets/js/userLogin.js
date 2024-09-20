98932d72068dd345d73e0bf85298d500db1932de
AngeKri <10511638@ucn.dk>
2024-09-20 10:06:49 +0200

    optimized JS

--- assets/js/userLogin.js
+++ assets/js/userLogin.js
@@ -2,18 +2,12 @@
 const dropdownBtn = document.querySelector(".dropdownBtn");
 const dropMenu = document.querySelector(".dropdownContent");
 
+
+// When button is clicked, run function "toggleDrop"
+dropdownBtn.addEventListener("click", toggleDrop);
+        
 // When function "toggleDrop" is activated, give dropdown div class "show"
-const toggleDrop = function() {
+function toggleDrop(){
     dropMenu.classList.toggle("show");
     console.log("clicked");
-}
-
-// toggleDrop();
-
-// When button is clicked, run function "eventDropdown" and "toggleDrop"
-dropdownBtn.addEventListener("click", function(eventDropdown){
-    // When function "eventDropdown" is activated, prevent parent element from being activated
-    // eventDropdown.preventDefault();
-    toggleDrop();
-})
-
+};
